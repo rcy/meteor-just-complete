20d8b55bb8ae300a820f3cafac9cd04e20709622
Ryan Yeske <rcyeske@gmail.com>
2014-04-16 01:29:20 -0700

    style

--- players.js
+++ players.js
@@ -3,8 +3,8 @@ Players = new Meteor.Collection('players');
 if (Meteor.isServer) {
   Meteor.startup(function () {
     Players.remove({});
-    Players.insert({name: 'wayne gretzky'});
-    Players.insert({name: 'bobby orr'});
-    Players.insert({name: 'gordie howe'});
+    Players.insert({name: 'Wayne Gretzky', team: 'Edmonton Oilers', number: 99});
+    Players.insert({name: 'Bobby Orr', team: 'Boston Bruins', number: 4});
+    Players.insert({name: 'Gordie Howe', team: 'Detroit Red Wings', number: 9});
   });
 }
