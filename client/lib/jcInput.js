4f9d5fba6532bd47f3d635b967a7753bbe6c78a2
Ryan Yeske <rcyeske@gmail.com>
2014-04-16 01:56:24 -0700

    add hidden field

--- client/lib/jcInput.js
+++ client/lib/jcInput.js
@@ -19,9 +19,17 @@ Template.jcInput.helpers({
 });
 
 Template.jcInput.events({
-  keyup: function (e) {
+  'keyup input.jc': function (e) {
     Session.set(this.query, $(e.target).val());
     console.log('keyup', this);
+  },
+  'click [data-match]': function (e, t) {
+    e.preventDefault();
+    var id = $(e.currentTarget).data('match');
+    var doc = window[t.data.collection].findOne(id);
+    $(t.find('input.jc')).val(doc.name);
+    $(t.find('input[type=hidden]')).val(doc._id);
+    Session.set(t.data.query);
   }
 });
 
